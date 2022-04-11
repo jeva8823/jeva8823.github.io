8f17a0f9cd68bba3d67304a86dae34b370bd2051
jeva8823 <jeva8823@colorado.edu>
2022-04-10 22:45:52 -0600

    Assignment 12

--- js/assignment12.js
+++ js/assignment12.js
@@ -1,39 +1,37 @@
-// document.querySelector('button'); (could apply to any button, not the best if you want something specific)
-// document.querySelector('.new-quote button'); (could apply to different elements, not the best choice)
-// document.querySelector('#js-new-quote'); (could be the best choice because the id is specific)
-// const means it doesn't change, then adding a variable (letters, _, $)
-//const quotebutton = document.querySelector('#js-new-quote');
-// using defer in html will allow script to load and doc to loas and script will run once everything else is done
-// console.log helps with debugging
-const endpoint = 'https://api.quotable.io/random';
-
-const quoteButton = document.querySelector('#js-new-quote');
-quoteButton.addEventListener('click', getQuote);
-
 async function getQuote() {
-  console.log("quote button was clicked");
-  try {
-    const response = await fetch(endpoint);
-    if (!response.ok) {
-      throw Error(response.statusText);
-    }
-    const json = await response.json();
-    console.log(json);
-    displayQuote(json.content);
-    displayA(json.author);
-    const tweetButton=document.getElementById("tweet");
+    console.log("quote button was clicked");
+    try {
+        const response = await fetch(endpoint);
+        if (!response.ok) {
+            throw Error(response.statusText);
+        }
+        const json = await response.json();
+        console.log(json);
+        displayQuote(json.content);
+        displayA(json.author);
+
+        const tweetButton=document.getElementById("tweet");
         tweetButton.href="https://twitter.com/intent/tweet?text="+json.content+" ~ "+json.author;
-  } catch(err) {
-    console.log(err)
-    alert('Failed');
-  }
+    }
+
+    catch(err) {
+        console.log(err);
+        alert('Failed');
+    }
+
 }
 
 function displayQuote(quote) {
-  const quoteText - document.querySelector('#js-quote-text');
-  quoteText.textContent = quote;
+    const quoteText = document.querySelector('#js-quote-text');
+    quoteText.textContent = quote;
 }
+
 function displayA(auth) {
     const quoteA = document.querySelector('#js-quote-auth');
     quoteA.textContent = "—" + auth;
 }
+
+const endpoint = 'https://api.quotable.io/random';
+
+const quoteButton = document.querySelector('#js-new-quote');
+quoteButton.addEventListener('click', getQuote);
---
+++ js/demo.js
@@ -0,0 +1,7 @@
+// document.querySelector('button'); (could apply to any button, not the best if you want something specific)
+// document.querySelector('.new-quote button'); (could apply to different elements, not the best choice)
+// document.querySelector('#js-new-quote'); (could be the best choice because the id is specific)
+// const means it doesn't change, then adding a variable (letters, _, $)
+//const quotebutton = document.querySelector('#js-new-quote');
+// using defer in html will allow script to load and doc to loas and script will run once everything else is done
+// console.log helps with debugging
